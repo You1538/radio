302eb0cd934d093f38e65115675af4b0c6bedf14
You1538 <137020294+You1538@users.noreply.github.com>
2023-11-27 11:45:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function getLocation() {
+    const isPermissionGranted = window.confirm("位置情報の利用を許可しますか？");
+
+    if (isPermissionGranted) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function(position) {
+//////////////////////////////////////////////////////////////////////////
 function createEntityElements(jsonData) {
     var entities = jsonData.members;
     var scene = document.getElementById('scene');
@@ -22,3 +30,27 @@ fetch('shibata_stores.json')
         createEntityElements(jsonData);
     })
     .catch(error => console.error('Error fetching JSON:', error));
+
+/////////////////////////////////////////////////////////////////////////////
+                },
+                function(error) {
+                    const errorMessage = "位置情報の取得に失敗しました。エラーコード: " + error.code + "\nエラーメッセージ: " + error.message;
+                    alert(errorMessage);
+                    console.error(errorMessage);
+                }
+            );
+        } else {
+            const errorMessage = "このブラウザは位置情報の取得をサポートしていません。";
+            alert(errorMessage);
+            console.error(errorMessage);
+        }
+    } else {
+        const errorMessage = "位置情報の利用が拒否されました。";
+        alert(errorMessage);
+        console.error(errorMessage);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    getLocation();
+});
